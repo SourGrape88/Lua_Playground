9e4b5e334ab3eaff9c34c4f100f476cda28bcee8
SourGrape88 <wallacebbarnes@gmail.com>
2026-03-08 22:08:26 -0500

    Added Animated Status Light

--- window.py
+++ window.py
@@ -1,6 +1,10 @@
 # ---------- WINDOW.PY -----------------------
 
-from PyQt6.QtWidgets import QMainWindow, QVBoxLayout, QHBoxLayout, QWidget, QPushButton, QPlainTextEdit
+# This Script Controls the Main UI Layout
+
+from PyQt6.QtWidgets import QMainWindow, QVBoxLayout, QHBoxLayout, QWidget, QPushButton, QPlainTextEdit, QLabel, QApplication
+from PyQt6.QtGui import QMovie, QPixmap
+from PyQt6.QtCore import QTimer
 from graphics_canvas import Canvas
 from code_editor import LuaEditor
 from lupa import LuaRuntime
@@ -17,27 +21,44 @@ class MainWindow(QMainWindow):
         # Pass Lua to Canvas
         self.canvas = Canvas(self.lua)
 
-        # Create the Text Editor
+        # Create the Code/Text Editor
         self.editor = LuaEditor()
 
-        
         # Create a "Run Button"
         self.run_button = QPushButton("Run")
+
+        # Create Status Indicator
+        self.status_icon = QLabel()
+        self.status_icon.setMinimumWidth(80)
+        self.status_icon.setFixedSize(64, 64)
+        self.status_icon.setScaledContents(True)
+        self.idle_icon = QMovie("assets/status_light/Idle_Status_Light.gif") # Idle GIF
+        self.run_icon = QMovie("assets/status_light/Running_Status_Light.gif") # Running GIF
+        self.run_icon.setSpeed(50)
+        self.finished_icon = QMovie("assets/status_light/Finished_Status_Light.gif") # Finished GIF
+        self.error_icon = QMovie("assets/status_light/Error_Status_Light.gif")
+        self.status_icon.setMovie(self.idle_icon)
+        self.idle_icon.start()
+
         # Create the Output Console
         self.console = QPlainTextEdit()
         self.console.setReadOnly(True)
         self.console.setMaximumHeight(100)
 
         # Layouts
-        main_layout = QVBoxLayout() # Vertical: Buttons + Horizontal + Console
+        main_layout = QVBoxLayout() # Vertical: Screens + Buttons + Console
         top_layout = QHBoxLayout() # Horizontal: Editor + Canvas
+        button_layout = QHBoxLayout() # Run + Status Buttons
 
         top_layout.addWidget(self.editor, stretch=1)
         top_layout.addWidget(self.canvas, stretch=1)
 
         main_layout.addLayout(top_layout)
-        main_layout.addWidget(self.run_button)
+        main_layout.addLayout(button_layout)
         main_layout.addWidget(self.console)
+
+        button_layout.addWidget(self.run_button, stretch=1)
+        button_layout.addWidget(self.status_icon)
 
         # Create a Central Widget
         central_widget = QWidget()
@@ -65,9 +86,28 @@ class MainWindow(QMainWindow):
         # Placeholder for now: Just print Editor Text to Console
         code = self.editor.text()
         try:
+            # Start "Running Code" Status Light
+            self.status_icon.setMovie(self.run_icon)
+            self.run_icon.start()
+
+            # Force the UI to Redraw
+            QApplication.processEvents()
+
             # Clear Previous Lua Draw Commands
             self.canvas.lua_draw_commands.clear()
             # Execute New Lua Code
             self.lua.execute(code)
+            # Start "Finished" Status Light
+            self.run_icon.stop()
+            self.status_icon.setMovie(self.finished_icon)
+            self.finished_icon.start()
+            QTimer.singleShot(2000, self.reset_status) # 1500 = 1.5 Seconds
         except Exception as e:
             self.console.appendPlainText(f"Lua Error: {e}")
+            self.status_icon.setMovie(self.error_icon)
+            self.error_icon.start()
+            #QTimer.singleShot(200, self.reset_status)
+
+    def reset_status(self):
+        self.status_icon.setMovie(self.idle_icon)
+        self.idle_icon.start()
